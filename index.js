3185a59759b0126ed6ec4cb3cb0813923219197e
joao-pedro-de-moura <jpmouraj@gmail.com>
2022-05-05 04:49:08 -0300

    commit de estrutura e base64 concluida

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-function cezar (){
-document.getElementById("cezar")
-if (cezar > 1)
-document.createElement("input.type=text")
+var selecionar = document.querySelector("#select")
+var chave = document.querySelector("#chaveDiv")
+selecionar.addEventListener("change",function(e){
+    if (selecionar.value == "2"){
+        chave.style = "display: flex"
+    } else {
+        chave.style = "display: none"
+    }
+    
+})
+var  btn = document.getElementById("btn")
+var entrada = document.getElementById("entrada")
+var select = document.getElementById("select")
+var saida = document.getElementById("saida")  
+var radio = document.getElementsByName("radio")   
 
 
-}
+
+// radio.addEventListener('change',function(){
+// if (valor == "codificar"){
+//     btn.innerText = ("codificar")
+// }
+//     else if (valor == "decodificar"){
+//         btn.innerText = ("decodificar")
+//     }
+// })
+btn.addEventListener('click',function(){
+    event.preventDefault()
+    var valor =  document.querySelector('input[name=codificar]:checked').value
+    var chave = document.getElementById("chave")
+     if (select.value == "1"){
+        if (valor == "codificar"){
+           
+        saida.innerText = (btoa(entrada.value))
+        }
+        else if (valor == "decodificar"){
+           
+            saida.innerText = (atob(entrada.value))
+        }
+       
+     }
+     else if (select.value == "2"){
+         
+        var cezinha = entrada.value.split('')
+        var cezar = ((+chave.value) + ((+cezinha.charCodeAt(0)) - 65) % 26)
+            console.log(cezar);
+          
+    }
+})
+
+
+
